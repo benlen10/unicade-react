58f306c61c0b2e9afd42df078d3ec84c5d502c18
Ben Lenington <ben2lening@gmail.com>
2024-06-06 17:21:18 -0500

    Use static data for displaying genres and platforms

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -10,9 +10,9 @@ interface Props {
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
-  if (error) return null;
+  // if (error) return null;
 
-  if (isLoading) return <Spinner />;
+  // if (isLoading) return <Spinner />;
 
   return (
     <>
